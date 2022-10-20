3b0943b12be290c4e24c7f5032158a9d31a7064e
Andrey Pechennikov <ap@devandy.me>
2022-10-20 15:19:16 +0300

    Fix seeder category and product generation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,13 +18,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Brand::factory(20)->create();
-        Category::factory(20)->create();
-        Product::factory(20)
-            ->create()
-            ->each(function (Product $product) {
-                $product
-                    ->categories()
-                    ->attach(Category::query()->inRandomOrder()->offset(rand(1, 3))->value('id'));
-            });
+        Category::factory(20)
+            ->has(Product::factory(rand(5, 20)))
+            ->create();
     }
 }
