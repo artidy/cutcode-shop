5e99a44d4be97bf32da2ea68749120c2af4af672
Andrey Pechennikov <ap@devandy.me>
2022-11-06 19:53:42 +0300

    Change slug generator

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -8,19 +8,53 @@ trait HasSlug
 {
     protected static function bootHasSlug(): void
     {
-        static::creating(function (Model $model) {
-            if ($model->slug) {
-                return;
-            }
-
-            $slug = str($model->{self::slugFrom()})->slug();
-            $countSameSlugs = $model->where('slug', $slug)->count();
-            $model->slug = $countSameSlugs === 0 ? $slug : "$slug-$countSameSlugs";
+        static::creating(function (Model $item) {
+            $item->makeSlug();
         });
+    }
+
+    protected function makeSlug(): void
+    {
+        $slug = $this->slugUnique(
+            str($this->{$this->slugFrom()})
+                ->slug()
+                ->value()
+        );
+
+        $this->{$this->slugColumn()} = $this->{$this->slugColumn()} ?? $slug;
+    }
+
+    protected function slugColumn(): string
+    {
+        return 'slug';
     }
 
     protected static function slugFrom(): string
     {
         return 'title';
+    }
+
+    private function slugUnique(string $slug): string
+    {
+        $originalSlug = $slug;
+        $count = 0;
+
+        while ($this->isSlugExists($slug)) {
+            $count++;
+
+            $slug = "$originalSlug-$count";
+        }
+
+        return $slug;
+    }
+
+    private function isSlugExists(string $slug): bool
+    {
+        $query = $this->newQuery()
+            ->where(self::slugColumn(), $slug)
+            ->where($this->getKeyName(), '!=', $this->getKey())
+            ->withoutGlobalScopes();
+
+        return $query->exists();
     }
 }
