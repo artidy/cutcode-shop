0024370946988b5d5f2c7a6a55e58a2068f8af44
Andrey Pechennikov <ap@devandy.me>
2022-11-10 13:23:11 +0300

    Add test for registration

---
+++ tests/Feature/App/Http/Controllers/AuthControllerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers;
+
+use App\Http\Controllers\AuthController;
+use App\Http\Requests\SignUpFormRequest;
+use App\Listeners\SendEmailNewUserListener;
+use App\Models\User;
+use App\Notifications\NewUserNotification;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_register_success(): void
+    {
+        Event::fake();
+        Notification::fake();
+
+        $request = SignUpFormRequest::factory()->create();
+
+        $this->assertDatabaseMissing('users',
+            ['email' => $request['email']]
+        );
+
+        $response = $this->post(
+            action([AuthController::class, 'signUp']),
+            $request
+        );
+
+        $response->assertValid();
+
+        $this->assertDatabaseHas('users',
+            ['email' => $request['email']]
+        );
+
+        Event::assertDispatched(Registered::class);
+        Event::assertListening(Registered::class, SendEmailNewUserListener::class);
+
+        $user = User::query()->where('email', $request['email'])->first();
+
+        $event = new Registered($user);
+
+        $listener = new SendEmailNewUserListener();
+        $listener->handle($event);
+
+        Notification::assertSentTo($user, NewUserNotification::class);
+
+        $this->assertAuthenticatedAs($user);
+
+        $response->assertRedirect(route('home'));
+    }
+}
