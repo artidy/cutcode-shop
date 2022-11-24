a2b5b3c7aa35b6c86a60b27dac8460d66feb5e86
Andrey Pechennikov <ap@devandy.me>
2022-11-24 21:15:56 +0300

    Add product in watched list

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,13 +6,15 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public function __invoke(?Product $product)
+    public function __invoke(Product $product)
     {
         $product->load(['optionValues.option']);
 
         $options = $product->optionValues->mapToGroups(function ($item) {
             return [$item->option->title => $item];
         });
+
+        session()->put('also.' . $product->id, $product->id);
 
         return view('product.show', [
             'product' => $product,
