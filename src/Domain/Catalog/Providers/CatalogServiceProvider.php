a613ad5cbbcb0966f4347f6824566353c59dbf7c
Andrey Pechennikov <ap@devandy.me>
2022-11-27 13:39:19 +0300

    Change sorting and filter modules

--- src/Domain/Catalog/Providers/CatalogServiceProvider.php
+++ src/Domain/Catalog/Providers/CatalogServiceProvider.php
@@ -5,6 +5,7 @@ namespace Domain\Catalog\Providers;
 use App\Filters\BrandFilter;
 use App\Filters\PriceFilter;
 use Domain\Catalog\Filters\FilterManager;
+use Domain\Catalog\Sorters\Sorter;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class CatalogServiceProvider extends ServiceProvider
@@ -23,5 +24,12 @@ class CatalogServiceProvider extends ServiceProvider
             new PriceFilter(),
             new BrandFilter()
         ]);
+
+        $this->app->bind(Sorter::class, function () {
+            return new Sorter([
+                'title',
+                'price'
+            ]);
+        });
     }
 }
